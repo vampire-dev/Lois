273d8a9302843e540d60397226129f48f31670ad
vampire-dev <vai2015dev@gmail.com>
2016-09-02 23:40:10 +0700

    add public files

---
+++ models/location.js
@@ -0,0 +1,13 @@
+﻿var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var objectId = mongoose.Types.ObjectId;
+var refId = mongoose.Schema.Types.ObjectId;
+
+var model = new Schema({
+    number: { type: Number, default: null },
+    name: { type: String, required: true },
+    prefix: { type: String, default: null },
+    region: { type: refId, ref: 'Region' }
+}, { versionKey: false, collection: 'locations' });
+
+module.exports = mongoose.model('Location', model);
