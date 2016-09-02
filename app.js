2c818aa7b14171c2c4f072fe08bcd867c5233cb4
vampire-dev <vai2015dev@gmail.com>
2016-09-02 23:36:25 +0700

    working models and controllers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 ﻿var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var config = require('./common').config();
 var app = express();
 
@@ -9,5 +10,11 @@ app.use(express.static(__dirname + '/public'));
 app.use(session({ secret: 'sdfe34234fdff234fsdf', saveUninitialized: true, resave: true }));
 
 app.listen(config.port, function (err) {
+    mongoose.connect(config.dsn);
+
+    mongoose.connection.on('connect', function (err) {
+        console.log('Database is running');
+    });
+
     console.log('Server is running on port ' + config.port);
 });
---
+++ controllers/region.js
@@ -0,0 +1,43 @@
+﻿var model = require('../models/region');
+var mongoose = require('mongoose');
+var co = require('co');
+var objectId = mongoose.Types.ObjectId;
+
+function Controller() { }
+
+Controller.prototype.get = function (id) {
+    return model.findOne({ "_id": objectId(id) });
+};
+
+Controller.prototype.getAll = function (query) {
+    var limit = query['limit'] ? query['limit'] : 10;
+    var skip = query['skip'] ? query['skip'] : 0;
+    var parameters = {};
+
+    if (query['name'])
+        parameters['name'] = new RegExp(query['name'], 'i');
+
+    return model.find(parameters).skip(skip).limit(limit).lean().exec();  
+};
+
+Controller.prototype.save = function (data) {
+    var entity = new model(data);
+
+    if (!data['_id'])
+        return entity.save();
+
+    return entity.update({ "_id": objectId(data['_id']), entity });
+};
+
+Controller.prototype.delete = function (id) {
+    return co(function* () {
+        var entity = model.findOne({ "_id": objectId(id) });
+
+        if (!entity)
+            throw new Error('Entity is not found');
+
+        return entity.remove({ "_id": objectId(id) });
+    });
+};
+
+module.exports = new Controller();
