36561232bdb24887ba19cb883f50fcca61896f34
vampire-dev <vai2015dev@gmail.com>
2016-09-03 00:55:36 +0700

    add shipping controller

---
+++ controllers/shipping.js
@@ -0,0 +1,12 @@
+﻿var model = require('../models/shipping');
+var mongoose = require('mongoose');
+var co = require('co');
+var objectId = mongoose.Types.ObjectId;
+
+function Controller() { }
+
+Controller.prototype.get = function (id) {
+    return model.findOne({ "_id": objectId(id) }).exec();
+};
+
+module.exports = new Controller();
--- models/shipping.js
+++ models/shipping.js
@@ -5,7 +5,86 @@ var refId = mongoose.Schema.Types.ObjectId;
 var model = new Schema({
     number: { type: Number, required: true },
     spbNumber: { type: String, requried: true },
-
+    date: { type: Date, default: null },
+    driver: { type: String, default: null },
+    receiver: {
+        name: { type: String, default: null },
+        address: { type: String, default: null },
+        contact: { type: String, default: null }
+    },
+    sender: { type: refId, ref: 'Client' },
+    destination: { type: refId, ref: 'Location' },
+    partner: { type: refId, ref: 'Partner' },
+    regions: {
+        source: { type: refId, ref: 'Region' },
+        destination: { type: refId, ref: 'Region' }
+    },
+    payment: {
+        type: { type: refId, ref: 'PaymentType' },
+        location: { type: refId, ref: 'Location' },
+        status: { type: String, default: 'Belum Terbayar' },
+        phases: [{
+            transferDate: { type: Date, default: null },
+            bank: { type: String, default: null },
+            notes: { type: String, default: null },
+            amount: { type: Number, default: 0 },
+            date: { type: Date, default: Date.now }
+        }],
+        paid: { type: Number, default: 0 }
+    },
+    cost: {
+        pph: { type: Number, default: 0.0 },
+        worker: { type: Number, default: 0.0 },
+        expedition: { type: Number, default: 0.0 },
+        total: { type: Number, default: 0.0 }
+    },
+    notes: {
+        shipping: { type: String, default: null },
+        partner: { type: String, default: null },
+        po: { type: String, default: null }
+    },
+    invoice: {
+        all: { type: String, default: null },
+        client: { type: String, default: null },
+        partner: { type: String, default: null }
+    },
+    tariff: { type: Number, default: 0 },
+    audited: { type: Boolean, default: false },
+    returned: { type: Boolean, default: false },
+    confirmed: { type: Boolean, default: false },
+    inputLocation: { type: refId, ref: 'Location' },
+    created: {
+        date: { type: Date, default: null },
+        user: { type: refId, ref: 'User' }
+    },
+    modified: {
+        date: { type: Date, default: Date.now },
+        user: { type: refId, ref: 'User' }
+    },
+    items: [{
+        itemType: { type: refId, ref: 'ItemType' },
+        packingType: { type: refId, ref: 'PackingType' },
+        content: { type: String, default: null },
+        dimensions: {
+            length: { type: Number, default: 0 },
+            width: { type: Number, default: 0 },
+            height: { type: Number, default: 0 },
+            weight: { type: Number, default: 0 }
+        },
+        colli: {
+            quantity: { type: Number, default: 0 },
+            available: { type: Number, default: 0 },
+            delivered: { type: Number, default: 0 }
+        },
+        cost: {
+            colli: { type: Number, default: 0 },
+            additional: { type: Number, default: 0 },
+            discount: { type: Number, default: 0 },
+            shipping: { type: Number, default: 0 }
+        },
+        status: { type: String, default: 'Belum Terekap' },
+        audited: { type: Boolean, default: false },
+    }]
 }, { versionKey: false, collection: 'shippings' });
 
 module.exports = mongoose.model('Shipping', model);
