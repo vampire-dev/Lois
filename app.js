4e4588a773d11eeecc40551fccec3224c60f5662
vampire-dev <vai2015dev@gmail.com>
2016-09-02 23:22:49 +0700

    init project

---
+++ app.js
@@ -0,0 +1,13 @@
+﻿var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var config = require('./common').config();
+var app = express();
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+app.use(session({ secret: 'sdfe34234fdff234fsdf', saveUninitialized: true, resave: true }));
+
+app.listen(config.port, function (err) {
+    console.log('Server is running on port ' + config.port);
+});
