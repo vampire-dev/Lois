9e195b2c4dee27ad774c5ab59b6e2be764b74bb3
root <root@debian>
2016-09-05 18:41:51 +0700

    add db dump

--- configurator.js
+++ configurator.js
@@ -1,6 +1,6 @@
 ﻿var env = require('./env.json');
 
 exports.config = function () {
-    var node_env = process.env.NODE_ENV || 'development';
+    var node_env = process.env.NODE_ENV || 'production';
     return env[node_env];
 };
